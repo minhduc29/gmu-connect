bd172bf93dce5f8e2d4758febb9f6878a08a4d9d
Duc Nguyen <ngmductm@gmail.com>
2025-04-16 16:34:59 -0400

    add MemberList component to chat for member operations

---
+++ frontend/src/components/chat/MemberList.jsx
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom'
+import { useState, useRef } from 'react'
+import { USERNAME } from '../../constants'
+
+function MemberList({ room, onAdd, onRemove }) {
+    const [content, setContent] = useState('')
+    const admin = useRef(room.admin?.username)
+    const username = useRef(localStorage.getItem(USERNAME))
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (content.trim()) {
+            onAdd(content.trim().split(/\s+/))
+            setContent('')
+        }
+    }
+
+    return (
+        <div className="chat-sidebar">
+            <h1 className="green">Members</h1>
+            {room.is_group &&
+                <form className="message-input" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="form-input"
+                        placeholder="Enter usernames to add (separated by spaces)"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)} />
+                    <button type='submit' className="chat-button green-button">Add</button>
+                </form>
+            }
+            <div className="room-list scroll-hidden">
+                {room.members.map((member, i) => (
+                    <div key={i} className="member-item">
+                        <Link to={`/profiles/${member.user.username}`} className="member-name">
+                            <span>{member.user.username} </span>
+                            <span className="role">{admin.current === member.user.username && '(Admin)'}</span>
+                        </Link>
+                        {room.is_group && member.user.username !== username.current && member.user.username !== admin.current && <button className="remove-button red-button" onClick={() => onRemove([member.user.username])}>X</button>}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default MemberList
