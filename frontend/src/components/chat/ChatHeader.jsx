40545d55f8e8d99b0305d422406ce18508bdf298
Duc Nguyen <ngmductm@gmail.com>
2025-04-16 23:41:49 -0400

    fix admin ref error

--- frontend/src/components/chat/ChatHeader.jsx
+++ frontend/src/components/chat/ChatHeader.jsx
@@ -1,12 +1,17 @@
+import { useRef } from "react"
+import { USERNAME } from "../../constants"
+
 function ChatHeader({ room, onLeave, onToggleMembers }) {
     if (!room) return null
+
+    const username = useRef(localStorage.getItem(USERNAME))
 
     return (
         <div className="chat-header">
             <h1 className="green">{room.display_name}</h1>
             <div className="header-actions">
                 <button className="chat-button green-button" onClick={onToggleMembers}>Members</button>
-                <button className="chat-button red-button" onClick={() => onLeave(room.id)}>Leave</button>
+                {room.is_group && room.admin?.username !== username.current && <button className="chat-button red-button" onClick={() => onLeave(room.id)}>Leave</button>}
             </div>
         </div>
     )
